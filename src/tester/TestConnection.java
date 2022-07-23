ee26929134fbf65e2d3a809eae8791e448b3094a
geetika142 <gkgeetikakumari75@gmail.com>
2022-07-23 20:15:20 +0530

    Hie

--- src/tester/TestConnection.java
+++ src/tester/TestConnection.java
@@ -8,6 +8,7 @@ public class TestConnection {
 		try(Connection cn=openConnection())
 		{
 			System.out.println("Connected to DB "+cn);//imple class instance : MySql specific cn 
+System.out.println("Hello");
 		}catch (Exception e) {
 			e.printStackTrace();
 		}
